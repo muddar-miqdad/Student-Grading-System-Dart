0b3666f4017ef428e906ba1a74f09f57dc3e9812
Ibrahiim-Ahmed <ibrahimahmed3647@gmail.com>
2023-04-25 01:42:41 +0500

    Portal Login

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main(){
+  print("Hello Please Fill in your credentials for further access \n");
+  print("Enter Your ID");
+  String? id = stdin.readLineSync();
+  print("Enter Your Password");
+  var pass = stdin.readLineSync();
+
+  if(id != "abc" || pass != "123a"){
+    print("Wrong ID/Password");
+  }
+  else{print("Welcome !!!");}
+
+  
+}
