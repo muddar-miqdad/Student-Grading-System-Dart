23099c679a3b4d6d4fd5be9c4785ba7f8808eec4
Ibrahiim-Ahmed <ibrahimahmed3647@gmail.com>
2023-04-25 02:27:31 +0500

    changes

--- main.dart
+++ main.dart
@@ -4,6 +4,7 @@ void main(){
   print("Hello Please Fill in your credentials for further access \n");
   print("Enter Your ID");
   String? id = stdin.readLineSync();
+  
   print("Enter Your Password");
   var pass = stdin.readLineSync();
 
